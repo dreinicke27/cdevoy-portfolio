104353326faec60f390af9cc43fbc104cd698640
dreinicke27 <danicareinicke@gmail.com>
2023-09-30 09:36:03 -0700

    added MyWork

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route} from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
+import MyWork from "./pages/MyWork";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          {/* <Route path="/mywork" element={<MyWork />} /> */}
         </Route>
     </Routes>
   );
---
+++ src/pages/MyWork.js
@@ -0,0 +1,9 @@
+const MyWork = () => {
+
+    return (
+        <h1>My Work</h1>
+    )
+
+}
+
+export default MyWork;
