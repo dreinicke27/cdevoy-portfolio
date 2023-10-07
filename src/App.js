2f0cd5ec68bfd9fcffc9736c452e59d69ceff570
dreinicke27 <danicareinicke@gmail.com>
2023-10-07 16:44:24 -0700

    fixing routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route exact path="contactcard" element={<Contact />} />
+          <Route path="contactcard" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Route>
     </Routes>
